21d54c4d109329ff95140fc053fa31de89f20d40
kanishyadav0001-creator <kanishyadav0001@gmail.com>
2026-05-17 17:11:50 +0530

    initial commit

---
+++ .py
@@ -0,0 +1,12 @@
+num = int(input("Enter Number to check :"))
+
+if num>50:
+    print("Number is greater than 50")
+    if num%2==0:
+        print("Add it is even too")
+    else:
+        print("Add it is odd")
+
+
+else:
+    print("Number is less than 50")    
